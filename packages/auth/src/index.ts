1ca795956c2a2e42850dfb861248622a12c0e006
Alexis Guzman <codingcodax@gmail.com>
2024-01-14 18:07:04 -0600

    feat(auth): add github provider

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,13 +2,14 @@ import type { DefaultSession } from 'next-auth';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth from 'next-auth';
 import Discord from 'next-auth/providers/discord';
+import GitHub from 'next-auth/providers/github';
 
 import { db, tableCreator } from '@acme/db';
 
 export type { Session } from 'next-auth';
 
 declare module 'next-auth' {
-  interface Session {
+  type Session = {
     user: {
       id: string;
     } & DefaultSession['user'];
@@ -22,7 +23,7 @@ export const {
   signOut,
 } = NextAuth({
   adapter: DrizzleAdapter(db, tableCreator),
-  providers: [Discord],
+  providers: [Discord, GitHub],
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
