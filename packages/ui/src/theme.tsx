f7dc8612190e5869eae0e8e8234f098b37eb6226
Alexis Guzman <codingcodax@gmail.com>
2024-01-25 20:52:47 -0600

    fix(ui): update button import

--- packages/ui/src/theme.tsx
+++ packages/ui/src/theme.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { ThemeProvider, useTheme } from 'next-themes';
 
-import { Button } from './button';
+import { Button } from './Button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -18,7 +18,7 @@ function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button size='icon' variant='outline'>
+        <Button intent='outline' size='small'>
           <SunIcon className='size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <MoonIcon className='absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
