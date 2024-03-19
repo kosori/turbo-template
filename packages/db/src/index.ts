17fa5a02a20693ed60e273f6a2193fb018317f12
Alexis Guzman <codingcodax@gmail.com>
2024-03-19 16:36:49 -0600

    feat(drizzle): use `pgTable` and `posgres`

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,16 +1,16 @@
-import { connect } from '@planetscale/database';
-import { drizzle } from 'drizzle-orm/planetscale-serverless';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 
 import * as auth from './schema/auth';
 import * as post from './schema/post';
 
 export const schema = { ...auth, ...post };
 
-export { mySqlTable as tableCreator } from './schema/_table';
+export { pgTable as tableCreator } from './schema/_table';
 
 export * from 'drizzle-orm';
 
-const connection = connect({
+const connection = postgres({
   host: process.env.DB_HOST!,
   username: process.env.DB_USERNAME!,
   password: process.env.DB_PASSWORD!,
